d6a64c0531eaebcec8d2f4e9ecc86d58ee9ccda9
Mathew Desjardins <desjardinsm@etsu.edu>
2022-02-21 19:03:31 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿System.Console.WriteLine("Give me a number between 1 - 10");
+int number1 = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Give me another number between 1 - 10");
+int number2 = int.Parse(Console.ReadLine());
+
+if(number1 < number2)
+{
+     System.Console.WriteLine($"{number1} is less then {number2}!");
+}
+
+else if(number1 > number2)
+{
+     System.Console.WriteLine($"{number1} is greater then {number2}!");
+}
+else 
+{
+    System.Console.WriteLine($"The two numbers ({number1} and {number2}) are equal!");
+}
+
+
+
+
